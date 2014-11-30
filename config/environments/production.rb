a69e5f59a03fca79d44c57efa32c0a9599c7de4e
Chris White <chris.matthew.white@gmail.com>
2014-11-30 12:16:27 -0500

    added devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,9 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # required for heroku
+  # need to set to host address
+  config.action_mailer.default_url_options = { :host => 'http://cwhite-pinteresting.herokuapp.com' }
+
 end
